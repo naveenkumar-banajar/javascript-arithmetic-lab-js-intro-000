0ef0b07a86a7fa96d4aa11dfadd3b6546e73fc77
Naveen kumar <naveenkumar.banajar@gmail.com>
2017-09-11 08:03:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var a,b
+function {
+  return add(a,b)
+}
+
+function sub(){
+  var a,b;
+  return subtract(a,b)
+}
+
+function mul(){
+  var a,b;
+  return multiply(a,b)
+}
+
+function div(){
+  var a,b;
+  return divide(a,b)
+}
